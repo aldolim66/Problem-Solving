f4e38c175dd56695ae1d19d695637c00c70fdbc5
aldolim66 <90307240+aldolim66@users.noreply.github.com>
2021-10-15 04:37:50 +0800

    Problem 8

---
+++ Assets/Scripts/BoxSetting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BoxSetting : MonoBehaviour
+{
+   
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Rigidbody2D>() == null)
+            return;
+
+        if(collision.gameObject.tag == "Ball")
+        {
+            ScoreManager.Instance.AddScore();
+            this.gameObject.SetActive(false);
+            Destroy(this.gameObject, 1);
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    #region Singleton
+    private static ScoreManager instance_ = null;
+    public static ScoreManager Instance
+    {
+        get
+        {
+            if (instance_ == null)
+            {
+                instance_ = FindObjectOfType<ScoreManager>();
+
+            }
+            return instance_;
+        }
+    }
+
+    #endregion
+
+    public Text scoreText;
+    public int currentScore;
+    private void Start()
+    {
+        currentScore = 0;
+    }
+    public void AddScore()
+    {
+        currentScore++;
+        scoreText.text = "Score: " + currentScore;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
