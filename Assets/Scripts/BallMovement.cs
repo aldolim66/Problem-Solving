e676ce181ff51afdfdab59110d023ceb923870e9
aldolim66 <90307240+aldolim66@users.noreply.github.com>
2021-10-14 20:17:56 +0800

    Problem 6

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    
+    public Camera Kamera;
+    public float speed;
+
+  
+    private void Update()
+    {
+        Vector3 mouse = Kamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+        Vector3 angle = mouse - transform.position;
+        
+        if(Vector3.Distance(mouse, transform.position) > 0.5)
+        {
+            transform.Translate(angle.normalized * speed * Time.deltaTime);
+        }
+    }
+
+
+}
