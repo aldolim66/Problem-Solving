2804906caf422f72be8f90b0b414ed7a3ccf2a0e
aldolim66 <90307240+aldolim66@users.noreply.github.com>
2021-10-15 03:17:08 +0800

    Problem 7

---
+++ Assets/Scripts/SpawnBox.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SpawnBox : MonoBehaviour
+{
+    public GameObject box;
+    public float left,right,bottom,top;
+
+
+    private void Start()
+    {
+        SpawnTheBox();
+    }
+    
+    void SpawnTheBox()
+    {
+        int Amount = Random.Range(5,25);
+        for(int i = 0; i < Amount; i++)
+        {
+            float randomx = Random.Range(left, right);
+            float randomy = Random.Range(top, bottom);
+            Instantiate(box, new Vector2(randomx, randomy), Quaternion.identity);
+        }
+    }
+
+}
