c71647c17b817d451b4938cd43562c521311d4c4
aldolim66 <90307240+aldolim66@users.noreply.github.com>
2021-10-15 13:28:52 +0800

    Problem 9

---
+++ Assets/Scripts/SpawnBox.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SpawnBox : MonoBehaviour
+{
+    public GameObject box;
+
+
+    private void Start()
+    {
+        SpawnTheBox();
+
+    }
+    
+    void SpawnTheBox()
+    {
+        int Amount = Random.Range(10,25);
+        for(int i = 0; i < Amount; i++)
+        {
+            float randomx = Random.Range(-3.6f, 3.6f);
+            float randomy = Random.Range(3.6f, -3.6f);
+            Instantiate(box, new Vector2(randomx, randomy), Quaternion.identity);
+        }
+    }
+
+    float SpawnTime = 3f;
+    public float SpawnTime2 = 1f;
+
+    void Update()
+    {
+        if (Time.time > SpawnTime)
+        {
+            float randomx = Random.Range(-3.6f, 3.6f);
+            float randomy = Random.Range(3.6f, -3.6f);
+            Instantiate(box, new Vector2(randomx, randomy), Quaternion.identity);
+
+            SpawnTime = Time.time + SpawnTime2;
+        }
+    }
+
+
+}
